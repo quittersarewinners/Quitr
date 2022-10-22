9a2a640bf4948c7730f878c0e1ce438b1c1f20df
Andrew <drewbusi123@gmail.com>
2022-10-22 15:31:06 -0400

    App Updated, Checkin foundation done, information started

---
+++ client/components/checkin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+const CheckIn = (props) => {
+    return (
+        <div className="checkIn">
+            {props.firstTime ? <h1>Are you ready to start your commitment?</h1> : <h1>Welcome back! Have you stayed away from your bad habit?</h1>}
+            <div className="checkInButtons">
+                <Link to='/info'>
+                    <button>Yes</button>
+                    <button>No</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default CheckIn
