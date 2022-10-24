716e23fd668178ddb948b4fc483bbc9448895d09
Rhea Wu <rheawu1212@gmail.com>
2022-10-24 17:09:30 -0400

    maybe final

--- client/components/InformationHeader.js
+++ client/components/InformationHeader.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const InformationHeader = (props) => {
+    console.log('user: ', props.user);
+    // const {first_name} = user;
+    // console.log('user.first_name: ', first_name);
     return (
         <div className="infoHeader">
-            <h1>Welcome Dhruv! You've been consistent for 1337 days!</h1>
+            <h1>Welcome {props.user ? props.user.first_name  : null} {props.user ? props.user.last_name : null}!</h1>
         </div>
     )
 }
--- client/components/UpcomingInfo.js
+++ client/components/UpcomingInfo.js
@@ -4,6 +4,11 @@ function UpcomingInfo() {
     return ( 
         <div className="upcomingInfo">
             <p>What you may expect in the next few days:</p>
+            <ul>
+              <li>Irritation</li>
+              <li>Better Breathing</li>
+              <li>Craving</li>
+            </ul>
         </div>
     )
 }
