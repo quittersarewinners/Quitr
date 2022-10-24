c4143287e56be63a0f8ecb83371c7bc2cdfa5a80
Stephen F <smf0211@gmail.com>
2022-10-24 10:59:52 -0500

    changed get route to use query param

--- client/components/CheckIn.js
+++ client/components/CheckIn.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-const CheckIn = (props) => {
-    return (
-        <div className="checkIn">
-            {props.firstTime ? <h1>Are you ready to start your commitment?</h1> : <h1>Welcome back! Have you stayed away from your bad habit?</h1>}
-            <div className="checkInButtons">
-                <Link to='/info'>
-                    <button>Yes</button>
-                    <button>No</button>
-                </Link>
-            </div>
-        </div>
-    )
-}
+const CheckIn = props => {
+  return (
+    <div className="checkIn">
+      {props.firstTime ? (
+        <h1>Are you ready to quit smoking!?</h1>
+      ) : (
+        <h1>Welcome back! Have you stayed away from your bad habit?</h1>
+      )}
+      <div className="checkInButtons">
+        <Link to="/info">
+          <button>Yes</button>
+          <button>No</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default CheckIn
+export default CheckIn;
