4794b84f73a1e30e6a7cd0a7a01b50630dd43f7f
Roman <rdarker@gmail.com>
2022-10-24 11:20:31 -0400

    habitSelector jist

--- client/components/habitSelector.js
+++ client/components/habitSelector.js
@@ -1,1 +1,32 @@
 import React from 'react';
+
+function ReadyToQuitr() {
+	return <p>Are you ready to QUITR?</p>;
+}
+
+function HabitOptions() {
+	return (
+		<select className="bad-habits">
+			<option value="smoking" />
+			<option value="also smoking" />
+			<option value="smoking as well" />
+			<option value="still smoking" />
+		</select>
+	);
+}
+
+function QuitButton() {
+	return <button className="ready-to-quit">I'm ready to quit!</button>;
+}
+
+function HabitSelector() {
+	return (
+		<div className="habit-selector">
+			<ReadyToQuitr />
+			<HabitOptions />
+			<QuitButton />
+		</div>
+	);
+}
+
+export default HabitSelector;
