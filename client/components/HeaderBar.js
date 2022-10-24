52794cc98d43da4c17737070189411e00f522671
Rhea Wu <rheawu1212@gmail.com>
2022-10-24 14:48:17 -0400

    styling with right-info-body connecting button

--- client/components/HeaderBar.js
+++ client/components/HeaderBar.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import logo from '../assets/logo_primary.png';
 
-const headerStyles = {
-  backgroundColor: '#3973e6',
-  margin: '0px',
-  padding: '0px',
-  width: '100%',
-  height: '8vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontFamily: 'Open Sans, sans-serif',
-  fontSize: 80,
-};
-
+// const headerStyles = {
+//   backgroundColor: '#3973e6',
+//   margin: '0px',
+//   padding: '0px',
+//   width: '100%',
+//   height: '8vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   fontFamily: 'Open Sans, sans-serif',
+//   fontSize: 80,
+// };
+//style={headerStyles}
 const HeaderBar = () => {
   return (
     <>
-      <div style={headerStyles}>Quitr</div>
+      <div className='headertest'>
+        <img src={logo} alt="logo"/>
+      </div>
     </>
   );
 };
