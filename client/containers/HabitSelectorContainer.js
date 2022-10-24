4b567266700b1076d8f25e6fc15ac03642633f30
Stephen F <smf0211@gmail.com>
2022-10-24 16:28:38 -0500

    fixed some things

--- client/containers/HabitSelectorContainer.js
+++ client/containers/HabitSelectorContainer.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {useState, useEffect} from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 // function ReadyToQuitr() {
@@ -7,40 +7,40 @@ import { Link } from 'react-router-dom';
 // }
 
 // function HabitOptions() {
-// 	return (
-// 		<select className="bad-habits">
-// 			<option value="smoking" />
-// 			<option value="also smoking" />
-// 			<option value="smoking as well" />
-// 			<option value="still smoking" />
-// 		</select>
-// 	);
+//   return (
+//     <select className="bad-habits">
+//       <option value="smoking" />
+//       <option value="also smoking" />
+//       <option value="smoking as well" />
+//       <option value="still smoking" />
+//     </select>
+//   );
 // }
 
 // function QuitButton() {
-	
+
 // 	return <button onClick={handleClick} className="ready-to-quit">I'm ready to quit!</button>;
 // }
 
 function HabitSelector() {
-	const handleClick = async () => {
-		await axios.post('/api/habit/create', {userId: 1, habitName: 'smoking'})
-	}
+  const handleClick = async () => {
+    await axios.post('/api/habit/create', { userId: 1, habitName: 'smoking' });
+  };
 
-	return (
-		<div className="habit-selector">
-			<p>Are you ready to QUITR?</p> 
-            <select className="bad-habits">
-                <option value="smoking" />
-                <option value="also smoking" />
-                <option value="smoking as well" />
-                <option value="still smoking" />
-		    </select>
-            <Link to="/info">
-            <button onClick={handleClick} className="ready-to-quit">I'm ready to quit!</button>
-            </Link>
-		</div>
-	);
+  return (
+    <div className="habit-selector">
+      <p>Are you ready to QUITR?</p>
+      <select className="bad-habits">
+        <option>Smoking</option>
+        <option>Smoking is the only option!</option>
+      </select>
+      <Link to="/info">
+        <button onClick={handleClick} className="ready-to-quit">
+          I'm ready to quit!
+        </button>
+      </Link>
+    </div>
+  );
 }
 
 export default HabitSelector;
