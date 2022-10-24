257215f6bd784fb84e09cfeb634182d018696f99
**L05Dhruv** <dhruv@dhruvs-mbp.lan>
2022-10-24 13:16:28 -0500

    Added getFact route in userRouter and userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,9 +12,45 @@ userController.getUser = async (req, res, next) => {
     res.locals.user = rows[0];
 
     return next();
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    return next({
+      status: error.status,
+      message: {
+        err: error.message
+      },
+      log: `Error in userController getUser middleware - ${error.log}`
+    });
   }
 };
+
+userController.getFact = async (req, res, next) => {
+  try {
+    const { userId, quitLength } = req.body;
+    const day = quitLength.days;
+    const str = `day_${day}`;
+    const queryString = 'SELECT FROM facts f WHERE f.fact_id = $1;';
+    const values = [str];
+    const { rows } = await db.query(queryString, values);
+    console.log(rows[0]);
+    res.locals.user = rows[0];
+
+    return next();
+  } catch (error) {
+    return next({
+      status: error.status,
+      message: {
+        err: error.message
+      },
+      log: `Error in userController getUser middleware - ${error.log}`
+    });
+  }
+};
+// Insert Queries for facts table:
+//  'INSERT INTO Facts (day_1) VALUES ('Your oxygen levels begin to return to normal, whilst nicotine and carbon monoxide levels in your blood decrease by over 50%');';
+//  'INSERT INTO Facts (day_2) VALUES ('You should start to notice an improved sense of taste and smell. As nicotine levels become depleted, the side effects of nicotine withdrawal such as anxiety and irritability might start to creep in.');';
+//  'INSERT INTO Facts (day_3) VALUES ('Your lungs begin to relax and breathing should be easier. Nicotine is completely eliminated from the body and as a result nicotine withdrawal symptoms will have reached their peak.');';
+//  'INSERT INTO Facts (day_7) VALUES ('The average smoker will begin to notice a reduction in the number of nicotine cravings experienced in a day (you’re getting there!)');';
+//  'INSERT INTO Facts (day_14) VALUES ('Your circulation starts to improve. You may notice that physical activity becomes a lot easier. You’ll be free of the addiction and any psychological effects of withdrawal should have ended.');';
+
 
 module.exports = userController;
