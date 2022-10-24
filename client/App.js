37cd412037f82b03d435197a15df6eb2c141b1ec
Andrew <drewbusi123@gmail.com>
2022-10-24 12:56:53 -0400

    Working on App.js

--- client/App.js
+++ client/App.js
@@ -1,7 +1,7 @@
 import "./app.css";
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 //App.js will hold header & habit selector
 import CheckIn from './components/CheckIn';
 import InformationContainer from './containers/InformationContainer'
@@ -10,7 +10,7 @@ import HabitSelectorContainer from "./containers/HabitSelectorContainer"
 const App = () => {
     //what hooks will we need? 
     const [firstTime, setFirstTime] = useState(false)
-    const [checkedIn, setCheckedIn] = useState(false)
+    const [checkedIn, setCheckedIn] = useState(true)
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -18,14 +18,21 @@ const App = () => {
         //to maintain most likely for now
     useEffect(() => {
         async function firstTimeChecker() {
-            const { data } = await axios.get('/api/habit', {userId: 2});
+            const { data } = await axios.get('/api/habit/1');
             //need to check if data retrieved is populated
             //if not populated, setFirstTime(true); which would then display the Habit Selector, and not the CheckIn
             //if populated, only the CheckIn should render
+            // if (!data) setFirstTime(true)
+            // console.log(firstTime)
 
             //if !firstTime, then need to check if user has checked in today within user object
             //if has checked in, setCheckedIn(true) -> redirect to '/info' path
             //if hasnt checked in (default), CheckIn will display automatically
+            if (!firstTime){
+                setCheckedIn(true);
+                window.location = '/info'
+            }
+
             console.log(data)
         }
         firstTimeChecker()
