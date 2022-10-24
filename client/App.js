f5aa95ef0900c69d22bbb9e288752b400d806d63
Andrew <drewbusi123@gmail.com>
2022-10-24 15:29:18 -0400

    Working on Render paths

--- client/App.js
+++ client/App.js
@@ -7,37 +7,30 @@ import CheckIn from './components/CheckIn';
 import InformationContainer from './containers/InformationContainer'
 import HeaderBar from "./components/HeaderBar";
 import HabitSelectorContainer from "./containers/HabitSelectorContainer"
+
+
 const App = () => {
     //what hooks will we need? 
-    const [firstTime, setFirstTime] = useState(false)
-    const [checkedIn, setCheckedIn] = useState(true)
-
+    const [firstTime, setFirstTime] = useState(true)
+    const [initialLoad, setInitialLoad] = useState(true)
+    const [hasCheckedIn, setHasCheckedIn] = useState(false)
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //on useEffect, will need to check if user has checked in today. will involve call on backend & state
-        //to maintain most likely for now
+        //to maintain
     useEffect(() => {
-        async function firstTimeChecker() {
+        //need function to get user data - will invoke within useEffect
+        async function getUserData(){
             const { data } = await axios.get('/api/habit/1');
-            //need to check if data retrieved is populated
-            //if not populated, setFirstTime(true); which would then display the Habit Selector, and not the CheckIn
-            //if populated, only the CheckIn should render
-            // if (!data) setFirstTime(true)
-            // console.log(firstTime)
-
-            //if !firstTime, then need to check if user has checked in today within user object
-            //if has checked in, setCheckedIn(true) -> redirect to '/info' path
-            //if hasnt checked in (default), CheckIn will display automatically
-            if (!firstTime){
-                setCheckedIn(true);
-                window.location = '/info'
-            }
-
+            if(data) setFirstTime(false)
             console.log(data)
         }
-        firstTimeChecker()
+        getUserData();
+        //if data is undefined, HabitSelectorContainer should render, if it does exist, it should not.
     }, []);
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
     return (
         <div className="forBG">
@@ -46,8 +39,8 @@ const App = () => {
             <Routes>
                 <Route path='/' element={
                     <>
-                        <HabitSelectorContainer />
-                        <CheckIn firstTime={firstTime} />
+                        {firstTime ? <HabitSelectorContainer setter={setFirstTime} /> : null}
+                        {!firstTime ? <CheckIn firstTime={firstTime} /> : null}
                     </>
                 } />
                 <Route path='/info' element={<InformationContainer />} />
@@ -56,17 +49,6 @@ const App = () => {
         </div>
     )
 }
-
-
-
-            {/* header component will go here <header.js />*/}
-            {/* React router routes will go here, dont have to setup routes until later.*/}
-            {/* -Habit selector will come first */}
-            {/* -Then Check in component */}
-            {/* -Then display component (do we seperate components within display?) */}
-
-
-
 
 
 export default App
