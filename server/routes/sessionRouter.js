5e952b6bde3ac09afa30bfa74035f8e524f97111
**L05Dhruv** <dhruv@dhruvs-mbp.lan>
2022-10-24 10:35:10 -0500

    Added Check In and New Day routes

---
+++ server/routes/sessionRouter.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+const sessionController = require('../controllers/sessionController');
+
+// router.get('/api/session', () => {});
+
+module.exports = router;
