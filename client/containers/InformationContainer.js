4d903505b9f42255c7b17d6935dcde6f32f981b5
Stephen F <smf0211@gmail.com>
2022-10-24 13:25:57 -0500

    added axios requests to info Container

--- client/containers/InformationContainer.js
+++ client/containers/InformationContainer.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import InformationHeader from '../components/InformationHeader'
+import React, { useEffect, useState } from 'react';
+import InformationHeader from '../components/InformationHeader';
 import LeftInfoBody from './LeftInfoBody';
 import RightInfoBody from '../components/RightInfoBody';
+import axios, { AxiosHeaders } from 'axios';
 
 //information header will require the use of pulling from db - username and habit quit time
-const InformationContainer = (props) => {
-    return (
-        <>
-            <InformationHeader />
-            <div className="infoBody">
-                <LeftInfoBody /> <RightInfoBody />
-            </div>
-        </>
-    )
-}
+const InformationContainer = props => {
+  const [habit, changeHabit] = useState();
+  const [user, changeUser] = useState();
+  const [fact, setFact] = useState();
 
+  useEffect(() => {
+    const getHabit = async () => {
+      const habitData = await axios.get('/api/habit/1');
+      const userData = await axios.get('/api/user?userId=1');
+      const factData = await axios.get('/api/user/fact', {
+        userId: 1,
+        quitLength: habitData.data.quitLength.days,
+      });
+      changeUser(userData.data);
+      changeHabit(habitData.data);
+      setFact(factData.fact);
+    };
+    getHabit().catch(err => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <InformationHeader user={user} />
+      <div className="infoBody">
+        <LeftInfoBody habit={habit} fact={fact} /> <RightInfoBody />
+      </div>
+    </>
+  );
+};
 
 export default InformationContainer;
--- client/containers/LeftInfoBody.js
+++ client/containers/LeftInfoBody.js
@@ -3,15 +3,17 @@ import QuitInfo from '../components/QuitInfo';
 import UpcomingInfo from '../components/UpcomingInfo';
 import HealthBenefits from '../components/HealthBenefits';
 
-function LeftInfoBody() {
-    return (
-        <div className="leftInfoBody">
-            <h1>Left side</h1>
-            <QuitInfo />
-            <UpcomingInfo />
-            <HealthBenefits />
-        </div>
-    );
+function LeftInfoBody(props) {
+  return (
+    <div className="leftInfoBody">
+      <h1>Left side</h1>
+      {props.habit ? props.habit.habit_name : null}
+      <QuitInfo />
+      <UpcomingInfo />
+      {props.fact ? props.fact : null}
+      <HealthBenefits fact={props.fact} />
+    </div>
+  );
 }
 
 export default LeftInfoBody;
