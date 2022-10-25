a05ba8fc18b6172bd270c0997a7bd82dab5c6fec
Stephen <sfadmin@goconfluent.com>
2022-10-25 09:07:43 -0500

    added some comments

--- client/containers/InformationContainer.js
+++ client/containers/InformationContainer.js
@@ -1,16 +1,18 @@
+import axios, { AxiosHeaders } from 'axios';
 import React, { useEffect, useState } from 'react';
 import InformationHeader from '../components/InformationHeader';
-import LeftInfoBody from './LeftInfoBody';
 import RightInfoBody from '../components/RightInfoBody';
-import axios, { AxiosHeaders } from 'axios';
+import LeftInfoBody from './LeftInfoBody';
 
-//information header will require the use of pulling from db - username and habit quit time
-const InformationContainer = props => {
+// This is the main container that will call the backend to pass the result of that information to the child components
+const InformationContainer = (props) => {
   const [habit, changeHabit] = useState();
   const [user, changeUser] = useState();
   const [fact, setFact] = useState();
 
   useEffect(() => {
+    // this calls the backend to get the habit, user, and corresponding fact.
+    // Due to lack of user auth userId 1 is hardcoded in most places.
     const getHabit = async () => {
       const habitData = await axios.get('/api/habit/1');
       const userData = await axios.get('/api/user?userId=1');
@@ -18,11 +20,13 @@ const InformationContainer = props => {
         `/api/user/fact?factId=${habitData.data.quitLength.days}`
       );
 
+      // call the setters on the state variables
       changeUser(userData.data);
       changeHabit(habitData.data);
       setFact(factData.data);
     };
-    getHabit().catch(err => console.log(err));
+    // Invoke the function. This is not semantaclly names=d
+    getHabit().catch((err) => console.log(err));
   }, []);
 
   return (
